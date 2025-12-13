2c6b43327f5acf15403147014506dfe7856d0115
yaroslav.andreiev@devsx.net <yaroslav.andreiev@devsx.net>
2025-12-13 19:09:50 +0100

    CAC-1 add manu with options

--- ConsoleAppCalculator/ConsoleAppCalculator/Program.cs
+++ ConsoleAppCalculator/ConsoleAppCalculator/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ShowMenu();
+            int option = int.Parse(Console.ReadLine());
         }
+
+        static void ShowMenu()
+        {
+            Console.WriteLine(
+                "Hello user, choose option:\n" +
+                "1. Add two numbers\n" +
+                "2. Subtract two numbers\n" +
+                "3. Multiply two numbers\n" +
+                "4. Divide two numbers\n" +
+                "5. Check number for even\n" +
+                "6. Calculate percentage from number\n" +
+                "7. Get square root from number\n" +
+                "8. Get power of number"
+            );
+        }
+
     }
 }
